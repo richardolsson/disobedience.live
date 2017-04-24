cffe90aabb125f6e71b4eca8fb7dc70e5643685e
Richard Olsson <r@richardolsson.se>
2017-04-24 13:24:31 +0200

    Add and style main menu

--- disobedience/functions.php
+++ disobedience/functions.php
@@ -3,4 +3,12 @@
 add_action('init', 'disobedience_init');
 function disobedience_init() {
     add_theme_support('post-thumbnails');
+
+    disobedience_register_nav_menus();
+}
+
+function disobedience_register_nav_menus() {
+    register_nav_menus(array(
+        'header-menu' => __('Header menu', 'disobedience'),
+    ));
 }
