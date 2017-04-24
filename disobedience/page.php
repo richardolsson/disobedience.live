288ec5cdf6bbe8ca1d1974aab3b9f5a3f2bd8b82
Richard Olsson <r@richardolsson.se>
2017-04-24 16:25:39 +0200

    Use hard-coded kicker instead of ACF

--- disobedience/page.php
+++ disobedience/page.php
@@ -5,14 +5,6 @@
         <?php the_post_thumbnail();?>
     </div>
     <h1><?php the_title();?></h1>
-    <div class="post-meta">
-        <p class="post-kicker">
-            <?php the_field('post_meta_kicker'); ?>
-        </p>
-        <p class="post-caption">
-            <?php the_field('post_meta_caption'); ?>
-        </p>
-    </div>
     <div class="post-content page-content">
         <?php the_content(); ?>
     </div>
