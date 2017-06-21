7d85fe19b83da867747cf44ff01068807837debe
Richard Olsson <r@richardolsson.se>
2017-06-21 13:38:47 +0200

    Add post types for activists and other voices

--- disobedience/functions.php
+++ disobedience/functions.php
@@ -5,10 +5,43 @@ function disobedience_init() {
     add_theme_support('post-thumbnails');
 
     disobedience_register_nav_menus();
+    disobedience_register_post_types();
 }
 
 function disobedience_register_nav_menus() {
     register_nav_menus(array(
         'header-menu' => __('Header menu', 'disobedience'),
+    ));
+}
+
+function disobedience_register_post_types() {
+    register_post_type('activist', array(
+        'labels' => array(
+            'name' => __('Activists', 'disobedience'),
+            'singular_name' => __('Activist', 'disobedience'),
+        ),
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail'
+        ),
+        'public' => true,
+        'has_archive' => false,
+    ));
+
+    register_post_type('voice', array(
+        'labels' => array(
+            'name' => __('Voices', 'disobedience'),
+            'singular_name' => __('Voice', 'disobedience'),
+        ),
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail'
+        ),
+        'public' => true,
+        'has_archive' => false,
     ));
 }
