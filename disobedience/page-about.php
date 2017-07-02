dd95dfd43c3b7259f62ce8e6506f3b264dad48be
Richard Olsson <r@richardolsson.se>
2017-07-02 14:06:48 +0200

    Create about page template

---
+++ disobedience/page-about.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: About Page
+ * Template Post Type: page
+*/
+
+get_header();
+the_post();
+
+$start_time = mktime(0, 0, 0, 8, 24, 2017);
+$now = mktime();
+$diff = $start_time - $now;
+$days = $diff / 60 / 60 / 24;
+?>
+<?
+
+$countdown = sprintf('<div class="countdown"><div class="gradient"></div><h2>%d %s remaining</h2></div>', $days, $days==1? 'day':'days');
+
+$html = apply_filters('the_content', get_the_content());
+$html = preg_replace('/<\/p>\s*<p>/', '</p>'.$countdown.'<p>', $html, 1);
+$tn_url = get_the_post_thumbnail_url();
+?>
+<div class="content post page about-page">
+    <h1 style="background-image: url(<?php echo $tn_url;?>)"><?php the_title();?></h1>
+    <div class="post-content page-content">
+        <?php echo $html; ?>
+    </div>
+</div>
+<?php get_footer();?>
