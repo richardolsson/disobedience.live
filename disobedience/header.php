2269292e5f7087c4f7b42a52edcdd8ecc6dbca53
Richard Olsson <r@richardolsson.se>
2017-07-05 11:22:18 +0200

    Add Alternative Gothic font

--- disobedience/header.php
+++ disobedience/header.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=170607">
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
         <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
+        <script src="https://use.typekit.net/rdb8ojx.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
         <meta name="viewport" content="width=device-width">
         <script
             src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
