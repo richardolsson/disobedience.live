1184b0a5c69e5c5a7eb8dcd9880a9c3d989e7a59
Richard Olsson <r@richardolsson.se>
2017-08-13 13:55:57 +0200

    Create template for activist pages

---
+++ disobedience/single-activist.php
@@ -0,0 +1,33 @@
+<?php the_post();?>
+<?php get_header();?>
+<?php
+    $facts = get_field('facts');
+?>
+<div class="content">
+    <?php the_post_thumbnail();?>
+    <div class="meta">
+        <h1><?php the_title();?></h1>
+        <?php if (!empty($facts)):?>
+        <div class="facts">
+            <ul class="fact-list">
+            <?php foreach ($facts as $fact):?>
+                <li class="fact-item">
+                    <span class="label"><?php echo $fact['label'];?></span>
+                    <span class="text"><?php echo $fact['text'];?></span>
+                </li>
+            <?php endforeach;?>
+            </ul>
+        </div>
+        <?php endif;?>
+        <div class="sharing">
+            <p class="share">
+                Share this activist bio:
+                <input class="share" type="text" value="<?php the_permalink();?>">
+            </p>
+        </div>
+    </div>
+    <div class="post-content">
+        <?php the_content(); ?>
+    </div>
+</div>
+<?php get_footer();?>
