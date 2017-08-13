7139fa64403e1aa11772a8a3f54dfa0e9964c033
Richard Olsson <r@richardolsson.se>
2017-08-13 11:18:16 +0200

    Move map initialization code to global javascript

--- disobedience/archive-event.php
+++ disobedience/archive-event.php
@@ -51,47 +51,9 @@
     <?php endwhile;?>
     ];
 
-    function initMarker(ev, map, bounds) {
-        var pos = new google.maps.LatLng(ev.lat, ev.lng);
-
-        bounds.extend(pos);
-
-        var image = {
-            url: '<?php echo get_template_directory_uri();?>/images/marker-green.png',
-            size: new google.maps.Size(64, 90),
-            scaledSize: new google.maps.Size(32, 45),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(16, 44),
-        };
-
-        var marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            icon: image,
-            title: ev.title,
-        });
-
-        marker.addListener('click', function() {
-            window.location = ev.link;
-        });
-    }
-
     function initMap() {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            scrollwheel: false,
-            zoom: 8,
-            styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative.country","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"},{"visibility":"on"},{"weight":8}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#282828"},{"weight":8}]},{"featureType":"administrative.locality","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text","stylers":[{"color":"#bdbdbd"},{"weight":8}]},{"featureType":"administrative.province","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative.province","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ff248a"}]},{"featureType":"landscape.man_made","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#fe6037"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#ff4856"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"color":"#ff28a3"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}],
-        });
-
-        var bounds = new google.maps.LatLngBounds();
-
-        for (var i = 0; i < events.length; i++) {
-            initMarker(events[i], map, bounds);
-        }
-
-        map.fitBounds(bounds);
+        var icons = ['<?php echo get_template_directory_uri();?>/images/marker-green.png'];
+        initEventMap(document.getElementById('map'), events, icons);
     }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNTh_Ay85-bSJ5WO1v-Sknl7R_IEBMVx4&callback=initMap" async defer></script>
