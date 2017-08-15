347e08711bdf83ed773d8fbb5a1fc1f7b24896ca
Richard Olsson <r@richardolsson.se>
2017-08-15 16:12:01 +0200

    Latest CSS build

--- disobedience/header.php
+++ disobedience/header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title><?php wp_title();?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708151147">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708151611">
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
         <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
         <script src="https://use.typekit.net/rdb8ojx.js"></script>
