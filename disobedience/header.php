1112a069faf68c9eb8488f50a1471f514b18b817
Richard Olsson <r@richardolsson.se>
2017-08-21 09:44:15 +0200

    Latest CSS build

--- disobedience/header.php
+++ disobedience/header.php
@@ -8,7 +8,7 @@
     <head>
         <?php wp_head();?>
         <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708171447">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708210944">
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
         <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
         <script src="https://dist.bambuser.net/player/lib/bambuser-video-iframeapi/latest/bambuser-video-iframeapi.min.js"></script>
@@ -19,7 +19,7 @@
             src="https://code.jquery.com/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
-        <script src="<?php echo get_template_directory_uri();?>/main.js?v=1708171447"></script>
+        <script src="<?php echo get_template_directory_uri();?>/main.js?v=1708210944"></script>
     </head>
     <body <?php body_class();?>>
         <div class="lang-switch">
