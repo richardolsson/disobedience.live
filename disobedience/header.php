87ee3b072083e63b9c4ffdba93b62f41092d4ff1
Richard Olsson <r@richardolsson.se>
2017-08-21 12:38:03 +0200

    Latest CSS build

--- disobedience/header.php
+++ disobedience/header.php
@@ -8,7 +8,7 @@
     <head>
         <?php wp_head();?>
         <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708210944">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708211237">
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
         <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
         <script src="https://dist.bambuser.net/player/lib/bambuser-video-iframeapi/latest/bambuser-video-iframeapi.min.js"></script>
