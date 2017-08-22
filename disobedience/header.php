b876c67f4afc184cc7fd2c6053a1bce907ee5fad
Richard Olsson <r@richardolsson.se>
2017-08-22 10:53:35 +0200

    Latest CSS and JS builds

--- disobedience/header.php
+++ disobedience/header.php
@@ -8,7 +8,7 @@
     <head>
         <?php wp_head();?>
         <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708211312">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css?v=1708221053">
         <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
         <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
         <script src="https://use.typekit.net/rdb8ojx.js"></script>
@@ -18,7 +18,7 @@
             src="https://code.jquery.com/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
-        <script src="<?php echo get_template_directory_uri();?>/main.js?v=1708210944"></script>
+        <script src="<?php echo get_template_directory_uri();?>/main.js?v=1708221053"></script>
     </head>
     <body <?php body_class();?>>
         <div class="lang-switch">
