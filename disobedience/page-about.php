d0e5a3c9b20367e4e6897bd6e32ebfaf476692ce
Richard Olsson <r@richardolsson.se>
2017-08-24 09:56:42 +0200

    Display time on about page

--- disobedience/page-about.php
+++ disobedience/page-about.php
@@ -16,13 +16,27 @@ if ($countdown_value < 1) {
     $countdown_value = round($diff / 60 / 60);
     $countdown_unit = 'hour';
 }
-?>
-<?
 
-$countdown = sprintf('<div class="countdown"><div class="gradient"></div><h2>%d %s%s remaining</h2></div>',
+if ($countdown_value > 0) {
+    $countdown = sprintf('<div class="countdown"><div class="gradient"></div><h2>%d %s%s remaining</h2></div>',
             $countdown_value,
             $countdown_unit,
             $countdown_value==1? '':'s');
+}
+else if ($countdown_value < -120) {
+    $countdown = '';
+}
+else {
+    $start_time = mktime(22, 0, 0, 8, 21, 2017);
+    $now = mktime();
+    $diff = $start_time - $now;
+    $countdown_value = abs($diff / 60);
+    $minutes = (abs($diff) / 60) % 60;
+    $hours = (abs($diff) / 60 / 60) % 24;
+    $day = floor(abs($diff) / 60 / 60 / 24);
+    $countdown = sprintf('<div class="countdown"><div class="gradient"></div><h2>DAY %d %02d:%02d</h2></div>',
+            $day, $hours, $minutes);
+}
 
 $html = apply_filters('the_content', get_the_content());
 $html = preg_replace('/<\/p>\s*<p>/', '</p>'.$countdown.'<p>', $html, 1);
